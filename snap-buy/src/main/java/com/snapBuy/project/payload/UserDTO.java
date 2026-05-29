d1d8ee25a7bb5ef51011d243783167c259a6f04c
Sadid Rafan <sadidrafan094@gmail.com>
2026-05-29 16:09:01 +0600

    payloads created

---
+++ snap-buy/src/main/java/com/snapBuy/project/payload/UserDTO.java
@@ -0,0 +1,26 @@
+package com.snapBuy.project.payload;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import com.snapBuy.project.model.Role;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+
+    private Long userId;
+    private String username;
+    private String email;
+    private String password;
+    private Set<Role> roles = new HashSet<>();
+    private AddressDTO address;
+    private CartDTO cart;
+
+}
